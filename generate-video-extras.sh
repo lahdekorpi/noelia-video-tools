9beedba30dc62cc91866e782db73d4d6bf58ab24
Toni Lähdekorpi <toni@lygon.net>
2014-05-05 18:58:31 +0000

    Initial commit

---
+++ generate-video-extras.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd /data/noelia/Julkiset-videot
+for i in *.mp4
+do
+	if [[ ! -f "${i%.mp4}.jpg" ]]
+	then
+		echo "${i%.mp4}.jpg" is not a file, imma gonna make one...
+		ffmpegthumbnailer -s 250 -i "${i}" -o "${i%.mp4}.jpg" -c jpeg
+		echo also a "${i%.mp4}.mini.jpg"
+		convert "${i%.mp4}.jpg" -resize "64x64^" -gravity center -crop 64x64+0+0 +repage "${i%.mp4}.mini.jpg"
+	fi
+
+        if [[ ! -f "mobile/${i%}" ]]
+	then
+		echo "mobile/${i%}" is not a file, imma gonna ffmpeg one for you...
+		/opt/ffmpeg -i "${i}" -vcodec libx264 -acodec aac -strict experimental -ac 2 -r 15 -ab 44100 -vf "scale=trunc((oh*a)/2)*2:240" mobile/"${i}"
+	fi
+
+	if [[ ! -f "webm/${i%.mp4}.webm" ]]
+	then
+		echo "webm/${i%.mp4}.webm" is not a file, imma gonna ffmpeg one for you. This gonna take a while...
+		/opt/ffmpeg -i "${i}" -quality good -cpu-used 0 -b:v 1M  -qmin 0 -qmax 50 -maxrate 500k -bufsize 1000k -threads 4 -codec:a libvorbis -b:a 128k webm/"${i%.mp4}.webm"
+	fi
+
+done
+
+chown -R www-data:www-data .
